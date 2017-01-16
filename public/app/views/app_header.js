be2ff75004a2151c42c85060f3ed9ddb7d831530
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-16 17:46:19 -0500

    change urls

--- public/app/views/app_header.js
+++ public/app/views/app_header.js
@@ -10,13 +10,13 @@ module.exports = React.createClass({
           <div className="container-fluid">
               <a className="navbar-brand" href="#"><img src="/img/logo.png" alt="" style={{width: '50%'}}/></a>
                <ul className="nav navbar-nav navbar-collapse">
-                  <li><a href="">Dashboard</a></li>
-                  <li><a href="/#!/quotation/create">Nueva Cotización</a></li>
-                  <li><a href="/#!/todos">Tareas</a></li>
-                  <li><a href="/#!/companies">Empresas</a></li>
-                  <li><a href="/#!/contacts">Contactos</a></li>
-                  <li><a href="/#!/services">Servicios</a></li>
-                  <li><a href="/#!/reports">Reporte</a></li>
+                  <li><a href="/">Dashboard</a></li>
+                  <li><a href="/quotation/create">Nueva Cotización</a></li>
+                  <li><a href="/todos">Tareas</a></li>
+                  <li><a href="/companies">Empresas</a></li>
+                  <li><a href="/contacts">Contactos</a></li>
+                  <li><a href="/services">Servicios</a></li>
+                  <li><a href="/reports">Reporte</a></li>
                   <li><a href="/logout">Cerrar sesion</a></li>
                </ul>
           </div>
