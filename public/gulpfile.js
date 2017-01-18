3f1026e01ddd6fe0174ffeffa2720b06af21972b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-18 17:18:25 -0500

    clean all dev dependecies

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -9,47 +9,6 @@ var sourcemaps = require('gulp-sourcemaps');
 var lodash = require('lodash');
 var babelify = require('babelify');
 
-// npm i -D watchify browserify gulp vinyl-source-stream vinyl-buffer gulp-util gulp-sourcemaps lodash babelify babel-preset-es2015 babel-preset-react
-
-// add custom browserify options here
-var customOpts = {
-  entries: ['./app/app.js'],
-   paths: [
-      './node_modules',
-      './app',
-      './app/lib',
-      './app/components',
-      './app/options/'
-    ],
-};
-
-var opts = lodash.assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts));
-b.transform(babelify, {presets: ["es2015", "react"]});
-
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-
-b.on('log', gutil.log); // output build logs to terminal
-
-function bundle() {
-  return b
-    .bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./js/dist'));
-
-}
-
-gulp.task('compile', bundle);
-
 // sass
 gulp.task('styles', function() {
     gulp.src('./public/sass/improve.scss')
