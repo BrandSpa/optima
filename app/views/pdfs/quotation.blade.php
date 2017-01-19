51029cea0869a67689eb5b49a8564a367f3a257b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-19 15:00:49 -0500

    add jquery

--- app/views/pdfs/quotation.blade.php
+++ app/views/pdfs/quotation.blade.php
@@ -53,7 +53,7 @@
 @stop
 
 <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script>
