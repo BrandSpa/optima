08964fb079853f182ec7816deebe89f178fa86a6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-19 15:24:06 -0500

    remove type

--- app/views/pdfs/quotation.blade.php
+++ app/views/pdfs/quotation.blade.php
@@ -62,7 +62,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script type='text/babel'>
+
+<script>
 	let Product = React.createClass({
 		render: function() {
 			return (
