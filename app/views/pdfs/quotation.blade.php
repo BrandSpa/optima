8fa655963993cc3df4777d6f1d98fb8840c73736
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-19 15:25:07 -0500

    add type

--- app/views/pdfs/quotation.blade.php
+++ app/views/pdfs/quotation.blade.php
@@ -55,6 +55,7 @@
 <div id="react"></div>
 
 <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.0.0/browser.js"></script>
 <script src="https://unpkg.com/react@15/dist/react.min.js"></script>
 <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
@@ -63,7 +64,7 @@
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 
-<script>
+<script type='text/babel'>
 	let Product = React.createClass({
 		render: function() {
 			return (
